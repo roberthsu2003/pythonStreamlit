624fbd6095d69c511ecb00ed936e209b7d090271
徐國堂 <roberthsu2003@gmail.com>
2023-08-11 05:00:47 +0000

    modify

---
+++ 實際案例/student_scores/README.md
@@ -0,0 +1,4 @@
+## 學生分數圖表
+### 顯示matplotlib的figure,靜態圖
+### 顯示plotly動態圖
+### streamlit 原生圖
--- 實際案例/student_scores/main.py
+++ 實際案例/student_scores/main.py
@@ -6,42 +6,100 @@
 #字型位置:/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
-font=FontProperties(fname='/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc',size=8)
-font1=FontProperties(fname='/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc',size=12)
-#window font=FontProperties(fname=r'c:\windows\fonts\simsun.ttc',size=8)
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
-student1 = {'國文':78, '英文':80, '數學':92, '歷史':75, '探索':85}
-student2 = {'國文':90, '英文':75, '數學':98, '歷史':65, '探索':75}
+@st.cache_data
+def getDataFrame():
+    student1 = {'國文':78, '英文':80, '數學':92, '歷史':75, '探索':85}
+    student2 = {'國文':90, '英文':75, '數學':98, '歷史':65, '探索':75}
+    return pd.DataFrame([student1, student2],index=["學生1","學生2"])
 
-subjects =list(student1.keys())
-value1 =list(student1.values())
-value2 = list(student2.values())
+@st.cache_data
+def getFigures(dataFrame:pd.DataFrame)->plt.figure:
+    font=FontProperties(fname='/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc',size=8)
+    font1=FontProperties(fname='/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc',size=12)
+    #window font=FontProperties(fname=r'c:\windows\fonts\simsun.ttc',size=8)
 
-fig = plt.figure(figsize=(8,5), dpi=250, facecolor="#cccccc")
-ax = fig.add_subplot(1,1,1)
-ax.set_title("學生1和學生2分數比較表",fontproperties=font)
-ax.set_xlabel("科目",fontproperties=font)
-ax.set_ylabel("分數",fontproperties=font)
-ax.plot(subjects,value1,'m-.o')
-ax.plot(subjects,value2,'c--o')
-ax.set_xticklabels(subjects,fontproperties=font)
+    subjects = dataFrame.columns
+    value1 = dataFrame.loc["學生1"]
+    value2 = dataFrame.loc["學生2"]
+
+    fig = plt.figure(figsize=(8,5), dpi=250, facecolor="#cccccc")
+    ax = fig.add_subplot(1,1,1)
+    ax.set_title("學生1和學生2分數比較表",fontproperties=font)
+    ax.set_xlabel("科目",fontproperties=font)
+    ax.set_ylabel("分數",fontproperties=font)
+    ax.plot(subjects,value1,'m-.o')
+    ax.plot(subjects,value2,'c--o')
+    ax.set_xticklabels(subjects,fontproperties=font)
 
 
-x_locations = ax.get_xticks()
-#y_locations = ax.get_yticks()
+    x_locations = ax.get_xticks()
+    #y_locations = ax.get_yticks()
 
-for i in range(len(x_locations)):
-    ax.annotate(str(value1[i]),(x_locations[i]+0.03,value1[i]))
-    ax.annotate(str(value2[i]),(x_locations[i]+0.03,value2[i])) 
-      
-value1_total = 0
-value2_total = 0
-for score in value1:
-    value1_total += score
+    for i in range(len(x_locations)):
+        ax.annotate(str(value1[i]),(x_locations[i]+0.03,value1[i]))
+        ax.annotate(str(value2[i]),(x_locations[i]+0.03,value2[i])) 
+        
+    value1_total = 0
+    value2_total = 0
+    for score in value1:
+        value1_total += score
 
-for score in value2:
-    value2_total += score
+    for score in value2:
+        value2_total += score
 
-ax.annotate(f'學生1總分{value1_total}\n學生2總分{value2_total}',(2.5,85),fontproperties=font)
-ax.legend([f"學生1:{value1_total}",f"學生2:{value2_total}"],prop=font) 
-plt.show()
+    ax.annotate(f'學生1總分{value1_total}\n學生2總分{value2_total}',(2.5,85),fontproperties=font)
+    ax.legend([f"學生1:{value1_total}",f"學生2:{value2_total}"],prop=font) 
+
+    return fig
+
+
+dataFrame = getDataFrame()
+st.dataframe(dataFrame.style)
+figure = getFigures(dataFrame)
+
+st.line_chart(dataFrame.T)
+
+#matplotlib figure
+st.pyplot(figure)
+
+#plotly's express
+import pandas as pd
+import plotly.express as px
+
+dataFrame1 = getDataFrame()
+dataFrame1 = dataFrame.T.stack().reset_index()
+dataFrame1.columns=["科目","姓名","分數"]
+fig1=px.line(dataFrame1,x='科目',y='分數',color='姓名',title="plotly Express")
+st.plotly_chart(fig1)
+
+import plotly.graph_objects as go
+
+#plotly
+# Create random data with numpy
+import numpy as np
+np.random.seed(1)
+
+N = 100
+random_x = np.linspace(0, 1, N)
+random_y0 = np.random.randn(N) + 5
+random_y1 = np.random.randn(N)
+random_y2 = np.random.randn(N) - 5
+
+# Create traces
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=random_x, y=random_y0,
+                    mode='lines',
+                    name='lines'))
+fig.add_trace(go.Scatter(x=random_x, y=random_y1,
+                    mode='lines+markers',
+                    name='lines+markers'))
+fig.add_trace(go.Scatter(x=random_x, y=random_y2,
+                    mode='markers', name='markers'))
+
+
+
+st.plotly_chart(fig)
