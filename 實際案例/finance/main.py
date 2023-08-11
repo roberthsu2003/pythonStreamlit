2390ae7787b09e849d9ae5e66ed015a2d849afb3
徐國堂 <roberthsu2003@gmail.com>
2023-08-11 12:47:31 +0000

    modify

--- 實際案例/finance/main.py
+++ 實際案例/finance/main.py
@@ -1,20 +1,42 @@
-import yfinance as yf
+import csv
+import pandas as pd
 import streamlit as st
+import ffn
 
 st.write("""
-# 簡易股票價格
-台積電股票價格
+# 股票交易價格
 """)
+@st.cache_data
+def getStockNames()->pd.Series:
+    with open('codeSearch.csv',encoding='utf-8',newline='') as file:
+        next(file)
+        csv_reader = csv.reader(file)
+        stock_codes = {}
+        for item in csv_reader:
+            key = item[2]
+            stock_codes[key] = item[3]
+    code_series = pd.Series(stock_codes)
+    return code_series
 
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
-#define the ticker symbol
-tickerSymbol = '2330.TW'
-#get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
-#get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2023-5-31')
-print(tickerDf)
-# Open	High	Low	Close	Volume	Dividends	Stock Splits
+def get_dataFrame(menu:list)->pd.DataFrame:
+    stock_data = ffn.get(menu)
+    return stock_data
 
-st.line_chart(tickerDf.Close)
-st.line_chart(tickerDf.Volume)
+#多重選取
+stockNames = getStockNames()
+stock_name_id = stockNames.index.to_numpy() + "_" + stockNames.values
+options = st.sidebar.multiselect('請選擇',
+                   stock_name_id,
+                   placeholder="股票:"  
+                       )
+names = []
+for name in options:
+    name_string = name.split('_')[0]
+    names.append(name_string+".TW")
+
+dataFrame:pd.DataFrame | None = None
+if len(names) != 0:
+    dataFrame = get_dataFrame(names)
+    st.dataframe(dataFrame)
+    st.line_chart(dataFrame)
+
